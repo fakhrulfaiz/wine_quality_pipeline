7f29d20fb02b3c7923def650244710d6913a6d3c
Fakhrul Faiz <fakhrulfaiz201@gmail.com>
2026-02-25 20:34:34 +0800

    update model training and transform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -24,3 +26,22 @@ try:
 except Exception as e:
     logger.exception(f"Error in stage {STAGE_NAME}: {e}")
     
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationPipeline()
+   data_transformation.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.initiate_model_training()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
